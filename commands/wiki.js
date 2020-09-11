00171ca6ab6a51f9a6f411a4e25ea9a8c5ab4a47
Rishab Jain <397630@bsd48.org>
2020-09-11 19:38:04 +0000

    underscore

--- commands/wiki.js
+++ commands/wiki.js
@@ -12,8 +12,8 @@ module.exports.run = async (client, message, args) => {
 	try {
     if (args.length < 1) {message.reply('Please specify a search term for the Among Us Wiki, e.g. `am!wiki mira hq`.'); return;}
     let ourstr = String(args);
-    ourstr = ourstr.replace(' ', '_')
-		message.channel.send(`https://among-us-wiki.fandom.com/wiki/${String(ourstr)}`);
+    ourstr = ourstr.split(" ");
+    message.channel.send(`https://among-us-wiki.fandom.com/wiki/${ourstr.join("_")}`);
 	}
 	catch (err) {
 		message.reply('Please give me the `EMBED LINKS` permission.');
