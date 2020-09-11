4c9cd12d25208367e7676793bb72cec33f55e6f8
Rishab Jain <397630@bsd48.org>
2020-09-11 19:28:53 +0000

    proper length

--- commands/wiki.js
+++ commands/wiki.js
@@ -5,12 +5,12 @@ module.exports = {
 	name: 'wiki',
 	description: 'Search the Among Us Fandom Wiki',
   usage: '[search term]',
-	cooldown: 3,
+	cooldown: 3
 };
 
 module.exports.run = async (client, message, args) => {
 	try {
-    if (!args) {message.reply('Please specify a search term for the Among Us Wiki, e.g. `am!wiki mira hq`.'); return;}
+    if (args.length < 1) {message.reply('Please specify a search term for the Among Us Wiki, e.g. `am!wiki mira hq`.'); return;}
     let ourstr = String(args);
     ourstr = ourstr.replace(' ', '_')
 		message.channel.send(`https://among-us-wiki.fandom.com/wiki/${ourstr}`);
