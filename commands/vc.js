a4fe19be5ee4f7088625eb324a07d6b3d309f32b
Rishab Jain <397630@bsd48.org>
2020-09-13 00:48:06 +0000

    vc

--- commands/vc.js
+++ commands/vc.js
@@ -10,28 +10,34 @@ module.exports.run = async (client, message, args) => {
     message.reply('you do not have access to create Private VCs. <:report:753870901749088256> This is exclusive for our Nitro Boosters.\nVisit <#754030174277861467> for more information.');
     return;
   };
-  const newTag = String(message.author.tag).replace('#', '').toLowerCase();
-  if(message.guild.channels.cache.find(channel => channel.name === newTag)){message.reply(`You already have a private VC created: #${newTag}. Please delete it before creating a new one.\n To delete the VC, right click and press \`Delete Channel\`.`);return;}
-  message.guild.channels.create(newTag, {
-    type: 'voice',
-    topic: `${message.author.tag}'s private voice channel.`,
-    bitrate: `64000`,
-    userLimit: `10`,
-    parent: message.guild.channels.cache.get('754446220168134787'),
-    permissionOverwrites: [
-      {
-        id: message.author.id,
-        allow: ['MANAGE_CHANNELS', 'VIEW_CHANNEL', 'CONNECT']
-      },
-      {
-        id: message.guild.roles.everyone,
-        deny: ['CONNECT', 'VIEW_CHANNEL']
-      }
-    ],
-  }).then(c=>message.reply(`I created channel <#${c.id}>. Invite your friends by right clicking and pressing 'Create Invite'. Have fun!\n **Do not rename the channel, otherwise it will be auto-deleted.**`))
+
+  const filter = m => m.author.id == message.author.id
+  message.react('✅');
+  message.channel.send(`What is the code for your lobby?\nType **cancel** to cancel.`).then(() => {
+    message.channel.awaitMessages(filter, { max: 1, time: 30000, errors: ['time'] })
+      .then(collected => {
+        if(collected.first().content.toLowerCase() == 'cancel'){message.reply('Cancelled creation!');return;}
+        message.guild.channels.create(`CODE: ${collected.first().content}`, {
+          type: 'voice',
+          topic: `${message.author.tag}'s private voice channel.`,
+          bitrate: `64000`,
+          userLimit: `10`,
+          parent: message.guild.channels.cache.get('754446220168134787'),
+          permissionOverwrites: [
+            {
+              id: message.author.id,
+              allow: ['MANAGE_CHANNELS', 'VIEW_CHANNEL', 'CONNECT']
+            },
+            {
+              id: message.guild.roles.everyone,
+              deny: ['CONNECT', 'VIEW_CHANNEL']
+            }
+          ],
+        })
+        message.reply(`I just created a Private VC for you, called **${collected.first().content}**.\nYou have permissions to edit the channel.\nTell friends to join the **Private Waiting Room**, and you can move them into your VC.\nPlease do not create more than 1 voice channel at a time.\n\nHave fun!`)
+      })
+      .catch(collected => {
+        message.reply('You did not respond with the game code.');
+      });
+  });
 };
-
-/**
- * ,
-
- */
