e6eba2227db3c061d41efe0bbaef148d1cd891a7
Rishab Jain <397630@bsd48.org>
2020-09-13 18:17:13 +0000

    aoa

--- dev/events.js
+++ dev/events.js
@@ -47,7 +47,7 @@ const msg = async (message, client, prefix, util) => {
   if (message.author.bot) {return;}
   if (message.channel.id == '747926089006841866' && message.mentions.users.has('286737962769580032')){
     message.reply('do not ping RJain. Further infractions will result in a mute.');
-    const u = client.users.cache.get('286737962769580032'); 
+    const u = message.guild.channels.cache.get('754767474452332584')
     u.send(`From ${message.author.tag}: ` + message.content)
     message.delete();
   }
