93c95be91ba0cf9a8cf1082d393f646205be4442
Rishab Jain <397630@bsd48.org>
2020-09-15 18:43:44 +0000

    zaab

--- commands/vc.js
+++ commands/vc.js
@@ -30,7 +30,7 @@ module.exports.run = async (client, message, args) => {
               permissionOverwrites: [
                 {
                   id: message.author.id,
-                  allow: ['MANAGE_CHANNELS', 'MANAGE_PERMISSIONS', 'VIEW_CHANNEL', 'CONNECT', 'MOVE_MEMBERS']
+                  allow: ['MANAGE_CHANNELS', 'VIEW_CHANNEL', 'CONNECT', 'MOVE_MEMBERS']
                 }
               ],
             })
