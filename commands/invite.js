66d59c157884c0613beb928e2a3b0c074a1bbfab
Rishab Jain <397630@bsd48.org>
2020-09-22 19:59:40 +0000

    proper id

--- commands/invite.js
+++ commands/invite.js
@@ -13,7 +13,7 @@ module.exports.run = async (_, message, __) => {
 			.setColor(embedColor)
 			.setAuthor('Invite Among Us to your Discord server!', footerImg)
 			.addFields(
-				{ name: '<:YTtools:715769246050680863> Invite Me', value: '[Get Among Us in Your Server!](https://discord.com/api/oauth2/authorize?client_id=754039067485929654&permissions=8&scope=bot)', inline: true },
+				{ name: '<:YTtools:715769246050680863> Invite Me', value: '[Get Among Us in Your Server!](https://discord.com/api/oauth2/authorize?client_id=758054448047063191&permissions=8&scope=bot)', inline: true },
 				{ name: '<:cash:729800059952037909> Support Server', value: '[Join for Help!](https://discord.gg/2nX2eaX)', inline: true },
 			)
 			.setTimestamp()
--- config.js
+++ config.js
@@ -1,12 +1,12 @@
 module.exports = {
     token: 'NzU4MDU0NDQ4MDQ3MDYzMTkx.X2pXOA.LBSkyqmc4U20v-1l1iwoWTBwqfc',
     prefix: "am!",
-    id: "754039067485929654",
+    id: "758054448047063191",
     footerTxt: "☕ Cafe Development © 2020",
     embedColor: 9043881,
     errors: {
         incorrectUsage: "For more help and a detailed description of each command, `ad help`"
     },
     footerImg: "https://lh3.googleusercontent.com/VHB9bVB8cTcnqwnu0nJqKYbiutRclnbGxTpwnayKB4vMxZj8pk1220Rg-6oQ68DwAkqO",
-  important: "[Invite Me](https://discord.com/api/oauth2/authorize?client_id=754039067485929654&permissions=8&scope=bot) - [Support Server](https://discord.gg/cGf3D2T)"
+  important: "[Invite Me](https://discord.com/api/oauth2/authorize?client_id=758054448047063191&permissions=8&scope=bot) - [Support Server](https://discord.gg/cGf3D2T)"
 };
