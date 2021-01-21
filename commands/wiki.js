9bca53ba11b1cf0a91d0f7572bb98448fada91d7
Rishab Jain <397630@bsd48.org>
2021-01-21 08:55:33 -0800

    Update wiki.js

--- commands/wiki.js
+++ commands/wiki.js
@@ -13,7 +13,7 @@ try {
     message.reply('Wiki command is disabled for maintenance.')
     return;
     if (message.mentions.everyone) {message.reply('Do not ping everyone.'); return;}
-    if (message.mentions.roles) {message.reply('Do not ping roles.'); return;}
+    if (message.content.includes('@')) {message.reply('Do not ping roles or users.'); return;}
     if (args.length < 1) {message.reply('Please specify a search term for the Among Us Wiki, e.g. `am!wiki Impostor`.'); return;}
     message.channel.send(`https://among-us-wiki.fandom.com/wiki/${String(message.content).slice(8).replace(" ", "_")}`);
 	}
