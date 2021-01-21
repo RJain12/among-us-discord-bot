4c9ae7e29d274d4e7eba4438929c3ab040207fab
Rishab Jain <397630@bsd48.org>
2021-01-21 08:53:02 -0800

    Update guide.js

--- commands/guide.js
+++ commands/guide.js
@@ -14,7 +14,7 @@ module.exports.run = async (client, message, args) => {
 		.setAuthor('Tips & Tricks. Strategy.', footerImg)
     .addFields(
       { name: '<:report:780281460837449771> Full Guide', value: '[Click Here](https://steamcommunity.com/sharedfiles/filedetails/?id=2220357997)', inline:true},
-		  { name: '<:blue_crewmate_gun:780619189340602388> Crewmate', value: '[Click Here](https://www.bluestacks.com/blog/game-guides/among-us/amongus-crewmate-guide-en.html)', inline:true },
+		  { name: '<:black_crewmate_knife:780619179593695303> Crewmate', value: '[Click Here](https://www.bluestacks.com/blog/game-guides/among-us/amongus-crewmate-guide-en.html)', inline:true },
       { name: '<:vent:780281601992818719> Impostor', value: '[Click Here](https://steamcommunity.com/sharedfiles/filedetails/?id=2220357997)', inline:true},
 		  { name: 'Important Links', value: important }
 		)
